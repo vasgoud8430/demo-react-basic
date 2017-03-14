97905136b3c032ef3500fd1c53cf35a879c79034
Ashwin <ashwin@Ashwins-MBP.fios-router.home>
2017-03-14 18:51:56 -0400

    Add input button to course page

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,11 +2,32 @@ import React from 'react';
 import {Link} from 'react-router';
 
 class CoursesPage extends React.Component{
+  constructor(props, context){
+    super(props,context);
+
+    this.state = {
+      course : {title:""}
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onSaveClick = this.onSaveClick.bind(this);
+  }
+  onTitleChange(event){
+    console.log('In title change');
+    const course = {title: event.target.value};
+    this.setState({course: course});
+  }
+  onSaveClick(){
+    console.log('Save', this.state.course.title);
+  }
 
   render(){
     return (
-      <div className="jumbotron">
+      <div>
         <h1>Courses Page</h1>
+        <h2>Add Course</h2>
+        <input type="text" onChange={this.onTitleChange} value={this.state.course.title}/>
+        <input type="submit" value="Save" onClick={this.onSaveClick}/>
       </div>
     );
   }
