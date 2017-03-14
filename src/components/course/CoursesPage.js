de08731e152f22504f751378bee7d1210d68d4b4
Ashwin <ashwin@Ashwins-MBP.fios-router.home>
2017-03-14 14:56:33 -0400

    first commit

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class CoursesPage extends React.Component{
+
+  render(){
+    return (
+      <div className="jumbotron">
+        <h1>Courses Page</h1>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
---
+++ tools/startMessage.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-console */
+import colors from 'colors';
+console.log('Server is started in dev mode.......'.green);
