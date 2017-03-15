1f3b4e6245f9be339830897906b397d97f0a76d7
Ashwin <ashwin@Ashwins-MBP.fios-router.home>
2017-03-14 22:45:18 -0400

    adding redux with react for courses exercise

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import {Link} from 'react-router';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component{
   constructor(props, context){
@@ -13,18 +16,23 @@ class CoursesPage extends React.Component{
     this.onSaveClick = this.onSaveClick.bind(this);
   }
   onTitleChange(event){
-    console.log('In title change');
     const course = {title: event.target.value};
     this.setState({course: course});
   }
   onSaveClick(){
-    console.log('Save', this.state.course.title);
+    //console.log('Save', this.state.course.title);
+    this.props.actions.createCourse(this.state.course);
+  }
+
+  courseRow(course, index){
+    return <div key={index}>{course.title}</div>;
   }
 
   render(){
     return (
       <div>
-        <h1>Courses Page</h1>
+        <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input type="text" onChange={this.onTitleChange} value={this.state.course.title}/>
         <input type="submit" value="Save" onClick={this.onSaveClick}/>
@@ -33,4 +41,22 @@ class CoursesPage extends React.Component{
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  //dispatch : PropTypes.func.isRequired,
+  //createCourse : PropTypes.func.isRequired,
+  courses : PropTypes.array.isRequired,
+  actions : PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps){
+  return {
+    courses : state.courses
+  };
+}
+function mapDispatchToProps(dispatch){
+  return {
+    actions : bindActionCreators(courseActions,dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
